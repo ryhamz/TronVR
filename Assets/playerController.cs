ffb069a25406d53a47c6817a27a6b3b3abde2f0d
ryhamz <ryhamz@gmail.com>
2016-02-14 21:32:26 -0600

    First commit

---
+++ Assets/playerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().AddRelativeForce (Vector3.right * 1000);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+}
