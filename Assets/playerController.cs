6a4cf0a018d53101cf0ad77c17c2fe51b8c7f80c
ryhamz <ryhamz@gmail.com>
2016-02-17 15:17:13 -0600

    collision with explosion. camera in 1st person

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -3,14 +3,29 @@ using System.Collections;
 
 public class playerController : MonoBehaviour {
 
+	public GameObject explosion;
+	public Transform playerTransform;
+
 	// Use this for initialization
 	void Start () {
-		GetComponent<Rigidbody>().AddRelativeForce (Vector3.right * 1000);
+		GetComponent<Rigidbody>().AddRelativeForce (Vector3.right * 2500);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
 
 	}
+
+	void OnTriggerEnter(Collider other) {
+
+		GetComponent<Rigidbody>().isKinematic = true;
+		GetComponent<Rigidbody>().detectCollisions = false;
+		Instantiate (explosion, playerTransform.position, playerTransform.rotation);
+		//Destroy (gameObject);
+		// Trying out this instead, because destroying the object also destroys the child camera.
+		GetComponent<Renderer>().enabled = false;
+	
+	}
+		
 }
